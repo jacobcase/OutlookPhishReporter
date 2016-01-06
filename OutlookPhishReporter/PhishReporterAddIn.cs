b8f32eeb4d4bac5955387085fefee21f13dc33d2
Jacob Case <jacobcase94@gmail.com>
2016-01-06 00:40:55 -0700

    Initial Commit

---
+++ OutlookPhishReporter/PhishReporterAddIn.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+
+namespace OutlookPhishReporter
+{
+    public partial class PhishReporterAddIn
+    {
+        //Edit these 2 strings and 1 array to contain all the values you want. mailRecipients can have multiple
+        //addresses if you want that for some reason.
+        private string mailSubjectPrefix = "[PhishReport] ";
+        private string[] mailRecipients = new string[] { "email@example.com" };
+        private string mailBody = "This is a user-submitted report of a phish email";
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new PhishReporterRibbon(this);
+        }
+
+
+        private void PhishReporterAddIn_Startup(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        private void PhishReporterAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        //This method gets the currently active window in Outlook, gets the selected items, iterates through
+        //them and attaches them to be submitted as phishing.  This is where all the magic happens.
+        public void SubmitSelectedEmails()
+        {
+            
+            Outlook.Explorer activeExplorere = this.Application.ActiveExplorer();
+            Outlook.Selection currentSelection = activeExplorere.Selection;
+
+            DialogResult dialogResult = MessageBox.Show(
+                "The selected emails will be submitted to your security team. Proceed?",
+                "Report Phishing?",
+                MessageBoxButtons.YesNo);
+
+            if (currentSelection.Count == 0)
+            {
+                MessageBox.Show("Please select messages to report and try again");
+                return;
+            }
+
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                foreach (object selected in currentSelection)
+                {
+                    Outlook.MailItem mailItem;
+                    //It may be possible that the selected item is not an email. I'm not sure under what
+                    //conditions this could occure, but better to handle it just in case.
+                    try
+                    {
+                        mailItem = (Outlook.MailItem)selected;
+                    }
+                    catch (InvalidCastException) { continue; }
+
+                    
+                    //An email is created that will be send to security and will have the phishing email attached.
+                    Outlook.MailItem submission = Application.CreateItem(Outlook.OlItemType.olMailItem);
+
+                    foreach (string recipient in mailRecipients)
+                    {
+                        submission.Recipients.Add(recipient);
+                    }
+
+                    submission.Subject = this.mailSubjectPrefix + mailItem.Subject;
+                    submission.Attachments.Add(mailItem, Outlook.OlAttachmentType.olEmbeddeditem);
+                    submission.Body = this.mailBody;
+                    submission.Send();
+                    mailItem.Delete();
+                }
+            }
+
+            
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(PhishReporterAddIn_Startup);
+            this.Shutdown += new System.EventHandler(PhishReporterAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ OutlookPhishReporter/PhishReporterRibbon.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Office = Microsoft.Office.Core;
+using System.Drawing;
+using System.Resources;
+
+namespace OutlookPhishReporter
+{
+    [ComVisible(true)]
+    public class PhishReporterRibbon : Office.IRibbonExtensibility
+    {
+        private string phishButtonLabel = "Report Phishing";
+        private string phishScreentip = "Report phishing emails";
+        private string phishSupertip = "Click this button to report the selected emails to the security team for phishing";
+
+
+        private Office.IRibbonUI ribbon;
+        private PhishReporterAddIn addin;
+
+        public PhishReporterRibbon(PhishReporterAddIn addin)
+        {
+            this.addin = addin;
+        }
+
+        //This method returns a text string of the xml for the ribbon icon which is the 2 xml files in the
+        //project.  Since when using the explorer and when viewing individual messages uses 2 different ribbons,
+        //to get the plugin to show up in both windows, you need to have a version for each ribbon.  The XML
+        //is nearly identical except for the idMso attribute of the <tab>.  This just identifies which window is 
+        //requesting the UI string and returns the approriate XML, or null if it's a non-applicable window.
+        public string GetCustomUI(string ribbonID)
+        {          
+            switch (ribbonID)
+            {
+                case "Microsoft.Outlook.Mail.Read":
+                    return GetResourceText("OutlookPhishReporter.PhishReporterRibbon.ReadMessage.xml");
+                case "Microsoft.Outlook.Explorer":
+                    return GetResourceText("OutlookPhishReporter.PhishReporterRibbon.Explorer.xml");
+                default:
+                    return null;                    
+            }
+        }
+
+        //Create callback methods here. For more information about adding callback methods, visit http://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void OnPhishClick(Office.IRibbonControl control)
+        {
+            addin.SubmitSelectedEmails();
+        }
+
+        public Bitmap OnPhishGetImage(Office.IRibbonControl control)
+        {
+            return Resources.PhishingIcon;
+        }
+
+        /*
+        I put all of these in callbacks rather than directly in the XML since
+        you will probably want the same string for all of the ribbons the button
+        will be on.
+        */
+        public string OnPhishLabel(Office.IRibbonControl control)
+        {
+            return this.phishButtonLabel;
+        }
+
+        public string OnPhishScreentip(Office.IRibbonControl control)
+        {
+            return this.phishScreentip;
+        }
+
+        public string OnPhishSupertip(Office.IRibbonControl control)
+        {
+            return this.phishSupertip;
+        }
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
